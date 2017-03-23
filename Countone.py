0881c070312cdc24f61924addbe74e90b6723f69
Clairepp <marionette.pc@gmail.com>
2017-03-23 19:46:34 -0400

    二进制中1的个数

---
+++ Countone.py
@@ -0,0 +1,18 @@
+#二进制中1的个数
+#输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+# -*- coding:utf-8 -*-
+class Solution:
+    def NumberOf1(self, n):
+        l = []
+        if (n < 0):
+            n = 2**32 + n
+        while n>0:
+            num = n % 2
+            l.append(num)
+            n = n //2
+             
+        count = 0
+        for i in range(len(l)):
+            if (l[i] == 1):
+                count += 1
+        return count
